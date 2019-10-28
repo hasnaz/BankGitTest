1b26fa05f3816d4a7a8bc23c6406e87e27dc5c3d
hasnaz <nazila.hasanzade@hkr.se>
2019-10-28 09:36:35 +0100

    Ändrat Hello world till Hello my team!

--- BankGitTest/Program.cs
+++ BankGitTest/Program.cs
@@ -6,7 +6,7 @@ namespace BankGitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello my team!");
         }
     }
 }
